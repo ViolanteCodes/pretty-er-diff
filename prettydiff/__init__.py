860b5a4bc16adedad652985eaa316c36e049be5c
ViolanteCodes <maria@mariadong.com>
2025-05-08 10:27:00 -0400

    add to init.py

--- prettydiff/__init__.py
+++ prettydiff/__init__.py
@@ -1,3 +1,3 @@
 from .diff_obj import diff_json, Diff, UNDEFINED
 from .prettydiff import get_annotated_lines_from_diff, Flag
-from .terminal import print_diff, Theme, DEFAULT_THEME
+from .terminal import print_diff, print_diff_differences, Theme, DEFAULT_THEME
